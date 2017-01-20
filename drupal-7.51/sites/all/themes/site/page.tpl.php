b1e2088cee4cbf3e1bfd37b4c2a5029c3007ad97
AlexCPU7 <alexcpu7@mail.ru>
2017-01-20 03:56:23 +0300

    All

--- drupal-7.51/sites/all/themes/site/page.tpl.php
+++ drupal-7.51/sites/all/themes/site/page.tpl.php
@@ -1,3 +1,9 @@
+<?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    <?php endif; ?>
+
 <?php if ($page['header']): ?>
     <div class="menu">
         <?php print render($page['header']); ?>
